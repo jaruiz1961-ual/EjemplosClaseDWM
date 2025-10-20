221ea5651d303ba819787ab5834130766155e29a
jose andres moreno ruiz <jaruiz@ual.es>
2025-10-20 21:39:20 +0200

    confirma

---
+++ Multitenancy/SingleDbSingleTable/Data/ContactContext.cs
@@ -0,0 +1,46 @@
+﻿using Common;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace SingleDbSingleTable.Data
+{
+    public class ContactContext : DbContext
+    {
+        private readonly int? _tenant = null;
+
+        public ContactContext(
+            DbContextOptions<ContactContext> opts,
+            ITenantService service)
+            : base(opts) => _tenant = service.Tenant;
+
+        public DbSet<Contact> Contacts { get; set; } = null!;
+
+        public async Task CheckAndSeedAsync()
+        {
+            if (await Database.EnsureCreatedAsync())
+            {
+                for (var i = 0; i < 10; i++)
+                {
+                    
+                    var tenant = (i % 2) + 1;
+                    var contact = $"Contact {i} (Tenant {tenant})";
+                    var cc = new Contact();
+                    cc.TenanId = tenant;
+                    cc.Name = contact;
+                    if (i == 3)
+                    {
+                        cc.Name = "Alice";
+                        cc.TenanId = 0;
+                    }
+                    Contacts.Add(cc);
+                }
+
+                await SaveChangesAsync();
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+            => modelBuilder.Entity<Contact>()
+                .HasQueryFilter(mt => mt.TenanId == _tenant || mt.TenanId==0);
+    }
+}
