69cdf848ba471ea759d6a8c5291110d0c810a98d
jose andres moreno ruiz <jaruiz@ual.es>
2025-10-21 20:52:13 +0200

    configuracion en maui

--- BlazorEbi9EF/MauiAppXalm9/MauiProgram.cs
+++ BlazorEbi9EF/MauiAppXalm9/MauiProgram.cs
@@ -8,6 +8,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using CommunityToolkit.Maui;
 using BlazorEbi9.Model.IServices;
+using System.Reflection;
+using Microsoft.Extensions.Configuration; // Asegúrate de tener este using
 
 namespace MauiAppXalm
 {
@@ -32,26 +34,65 @@ namespace MauiAppXalm
 #if DEBUG
     		builder.Logging.AddDebug();
 #endif
-            //if (DeviceInfo.Platform == DevicePlatform.Android)
-            //{
-            //    builder.Services.AddSingleton<IHttpsClientHandlerService, HttpsClientHandlerService>();
-            //    var HttpsClientHandS = builder.Services.BuildServiceProvider().GetService<IHttpsClientHandlerService>() as IHttpsClientHandlerService;
-            //    builder.Services.AddScoped<IUsuarioServiceAsync>(sp => new UsuarioServiceR(HttpsClientHandS, RestUrl));
-            //}
-            //else
-            //    builder.Services.AddScoped<IUsuarioServiceAsync>(sp => new UsuarioServiceR(new HttpClient(), RestUrl));
+            // propiedades del arcivo json .. tiene que estar incrustado como recurso
+            string tipoAcceso = "";
+            var assembly = Assembly.GetExecutingAssembly();
+            var assemblyName = assembly.GetName().Name;
+            var appSettingsFileName =  $"{assemblyName}.appsettings.json";
+            using var stream = assembly.GetManifestResourceStream(appSettingsFileName);
+            if (stream != null)
+            {
+                var config = new ConfigurationBuilder()
+                   .AddJsonStream(stream)
+                   .Build();
+               
 
+                // 5. Agregar la configuración al Builder
+                builder.Configuration.AddConfiguration(config);
+            }
+            var lectura = builder.Configuration["DataSettings:DataSourceType"];
+            tipoAcceso = lectura ?? "API";
 
-            var DatabasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "miSqlitedb");
+            if (tipoAcceso == "API")
+            {
 
-            builder.Services.AddDbContext<SqLiteDbContext>(options => options.UseSqlite($"FileName={DatabasePath}"));
-            builder.Services.AddTransient<IUnitOfWorkAsync, UnitOfWorkAsync<SqLiteDbContext>>();
-            
+                if (DeviceInfo.Platform == DevicePlatform.Android)
+                {
+                    LocalhostUrl = "10.0.2.2";
+                    Scheme = "https"; 
+                    Port = "5001";
+                    RestUrl = $"{Scheme}://{LocalhostUrl}:{Port}/api/";
+                    builder.Services.AddSingleton<IHttpsClientHandlerService, HttpsClientHandlerService>();
+                    var HttpsClientHandS = builder.Services.BuildServiceProvider().GetService<IHttpsClientHandlerService>() as IHttpsClientHandlerService;
+                    builder.Services.AddScoped<IUsuarioServiceAsync>(sp => new UsuarioServiceR(HttpsClientHandS, RestUrl));
+                }
+                else
+                {
+                    LocalhostUrl = "localhost";
+                    Scheme = "https";
+                    Port = "5001";
+                    RestUrl = $"{Scheme}://{LocalhostUrl}:{Port}/api/";
+                    builder.Services.AddScoped<IUsuarioServiceAsync>(sp => new UsuarioServiceR(new HttpClient(), RestUrl));
+                }
+            }
+            else if (tipoAcceso == "SQLITE")
+            {
+                var miSqlitedb = builder.Configuration["ConnectionStrings:SQLiteConnection"]?? "miSqlitedb";
+                var DatabasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), miSqlitedb);
+                builder.Services.AddDbContext<SqLiteDbContext>(options => options.UseSqlite($"FileName={DatabasePath}"));
+                builder.Services.AddTransient<IUnitOfWorkAsync, UnitOfWorkAsync<SqLiteDbContext>>();
 
-            builder.Services.AddTransient<IUsuarioServiceAsync, UsuarioServiceAsync>();
+                builder.Services.AddTransient<IUsuarioServiceAsync, UsuarioServiceAsync>();
+            }
 
             builder.Services.AddSingleton<ListaUsuariosPage>();
             builder.Services.AddTransient<DetalleUsuarioPage>();
+
+            // leer del archivo de configuracion embebido
+     
+
+
+
             return builder.Build();
         }
     }
