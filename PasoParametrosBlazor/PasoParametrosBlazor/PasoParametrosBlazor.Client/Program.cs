75eae76800dc453618b293d984119f3fcf9f1c3b
moreno ruiz jose andres (jaruiz) <jaruiz@inlumine.ual.es>
2025-11-10 09:55:19 +0100

    actualizacion desde universidad pasoParametrosBlazor

--- PasoParametrosBlazor/PasoParametrosBlazor/PasoParametrosBlazor.Client/Program.cs
+++ PasoParametrosBlazor/PasoParametrosBlazor/PasoParametrosBlazor.Client/Program.cs
@@ -1,9 +1,14 @@
 using Blazored.LocalStorage;
 using LibreriaCompartida;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+<<<<<<< HEAD
 using PasoParametrosBlazor.Client;
+=======
+using RazorClassLibrary;
+>>>>>>> actualizacion desde universidad pasoParametrosBlazor
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddSingleton<AppState>();
 
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddScoped<AppState>();
