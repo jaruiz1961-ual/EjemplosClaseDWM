170bc0b251e2c57036d98b1fe6b11f90bee0f952
jose andres moreno ruiz <jaruiz@ual.es>
2025-11-21 19:51:34 +0100

    borrar antigua

--- BlazorAppEFTenant/ConsoleApp1/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
-
-Console.WriteLine("Hello, World!");
-using var scope = app.Services.CreateScope();
-var ctx = scope.ServiceProvider.GetRequiredService<AppDbContext>(); // o SqlDbContext real
-bool ok = await ctx.Database.CanConnectAsync();
-Console.WriteLine("CanConnect = " + ok);
