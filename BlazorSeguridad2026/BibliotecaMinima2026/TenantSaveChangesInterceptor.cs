fec270515e46854645e37d9b00d74b218d542870
jose andres moreno ruiz <jaruiz@ual.es>
2025-12-25 18:48:04 +0100

    Compila-con cnuevo proyectos

--- BlazorSeguridad2026/BibliotecaMinima2026/TenantSaveChangesInterceptor.cs
+++ BlazorSeguridad2026/BibliotecaMinima2026/TenantSaveChangesInterceptor.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using BlazorSeguridad2026.Base.Modelo;
 
+
 namespace BlazorSeguridad2026.Base.Genericos
 {
     public class TenantSaveChangesInterceptor : SaveChangesInterceptor
